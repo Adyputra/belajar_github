abfbb635d91dd9c78db9da0743d97a06492ab23b
Ady <Ady@DESKTOP-E1KJ85>
2022-05-30 10:44:04 +0700

    terbaru2

--- API.php
+++ API.php
@@ -14,6 +14,7 @@ class API extends CI_Controller
         );
         $this->cart->update($data);
         echo 'Berhasil Dihapus';
+        echo 'haloo'
     }
 
     public function tampilProvinsi()
